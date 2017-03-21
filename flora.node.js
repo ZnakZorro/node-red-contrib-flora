6ff85c952bd539aace0f7a76a9dd84c068e27737
Znak Zorro <ZnakZorro@users.noreply.github.com>
2017-03-21 18:39:40 +0100

    Update flora.node.js

--- flora.node.js
+++ flora.node.js
@@ -5,10 +5,11 @@ module.exports = function(RED) {
         RED.nodes.createNode(this,config);
         this.name = config.name;
         this.repeat = config.repeat;
+        this.timerepeat = config.timerepeat;
         this.interval = config.interval;
         var node = this;
         this.on('input', function(msg) {
-            msg.payload = msg.payload.toLowerCase() + ' '+this.name + '  '+ this.repeat + '  '+ this.interval;
+            msg.payload = msg.payload.toLowerCase()+' '+this.name+'  '+this.repeat+' '+this.timerepeat+' '+this.interval;
             node.send(msg);
             this.status({fill:"red",shape:"ring",text:"try connect"});
         });

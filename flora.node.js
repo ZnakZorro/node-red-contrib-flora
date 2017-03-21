ef3bf10632f4502af5fd58d83acea9ce7a9e5ee0
Znak Zorro <ZnakZorro@users.noreply.github.com>
2017-03-21 10:09:02 +0100

    Update flora.node.js

--- flora.node.js
+++ flora.node.js
@@ -1,1 +1,13 @@
 /*flora.node.js*/
+/* only example from tutorial*/
+module.exports = function(RED) {
+    function LowerCaseNode(config) {
+        RED.nodes.createNode(this,config);
+        var node = this;
+        this.on('input', function(msg) {
+            msg.payload = msg.payload.toLowerCase();
+            node.send(msg);
+        });
+    }
+    RED.nodes.registerType("lower-case",LowerCaseNode);
+}

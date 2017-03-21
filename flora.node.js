ad256c825667bdd90c202ac7a9e7d3276310a19a
Znak Zorro <ZnakZorro@users.noreply.github.com>
2017-03-21 10:11:14 +0100

    Update flora.node.js

--- flora.node.js
+++ flora.node.js
@@ -9,5 +9,5 @@ module.exports = function(RED) {
             node.send(msg);
         });
     }
-    RED.nodes.registerType("lower-case",LowerCaseNode);
+    RED.nodes.registerType("flora",LowerCaseNode);
 }
